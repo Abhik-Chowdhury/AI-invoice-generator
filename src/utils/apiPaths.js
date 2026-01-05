7e47e9811bdf23550833beb7cc3481bd7247ab20
unknown <abhikc96@gmail.com>
2026-01-05 12:35:19 +0530

    add apiPath changes

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://invoice-api-ai.onrender.com";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://invoice-api-ai.onrender.com";
+// export const BASE_URL = "http://localhost:5000";
 
 export const API_PATHS = {
     AUTH: {
